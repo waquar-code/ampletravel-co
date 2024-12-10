21366e904e694604576680870d3cb0e3f8aff777
notthewaquar <notthewaquar@gmail.com>
2024-12-10 15:08:36 +0530

    fix

--- src/app/partial/flight-search/flight-search.component.ts
+++ src/app/partial/flight-search/flight-search.component.ts
@@ -14,6 +14,15 @@ export class FlightSearchComponent {
     flight_from: new FormControl('McCarran Intl, US (LAS)', [
       Validators.required,
     ]),
+    flight_to: new FormControl('McCarran Intl, US (LAS)', [
+      Validators.required,
+    ]),
+    flight_dept: new FormControl('', [Validators.required]),
+    flight_return: new FormControl('', [Validators.required]),
+    flight_adult: new FormControl(1, [Validators.required]),
+    flight_child: new FormControl(0, [Validators.required]),
+    flight_infant: new FormControl(0, [Validators.required]),
+    flight_coach: new FormControl('economy', [Validators.required]),
   });
 
   constructor(
@@ -24,6 +33,20 @@ export class FlightSearchComponent {
 
   ngOnInit() {
     this.setFlightFormValues();
+  }
+
+  guestCounter(event: any, field: string, mode: boolean) {
+    event.stopPropagation();
+
+    let value = this.flightForm.get(field)?.value;
+    mode ? value++ : value--;
+    if (value <= 0) value = 0;
+
+    this.flightForm.patchValue({ [field]: value });
+  }
+
+  openDropdown(event: any) {
+    console.log(event);
   }
 
   setFlightFormValues() {
